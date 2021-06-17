f87a689fa6001ef0a38ef64aaae25656bc2baf8a
Jeff Lill <jeff@lilltek.com>
2021-06-17 12:35:17 -0700

    debugging...

--- action.ps1
+++ action.ps1
@@ -110,6 +110,11 @@ try
         $xenPassword         = Get-SecretValue  "$xenCredentialsName[password]" $xenCredentialsVault
         $xenserverIsRunning  = Check-XenServer  $xenHostIP $xenUsername $xenPassword
 
+Log-DebugLine "xenHostIP:   $xenHostIP"
+Log-DebugLine "xenUsername: $xenUsername"
+Log-DebugLine "xenPassword: $xenPassword"
+Log-DebugLine "xenOwner:    $xenOwner"
+
         if ($xenserverIsRunning)
         {
             Remove-XenServerVMs $xenHostIP $xenUsername $xenPassword "$xenOwner-*"
