614fdff9e97e0c770522ee70fe838f6c5cddc943
Jeff Lill <jeff@lilltek.com>
2021-06-22 21:31:00 -0700

    debugging...

--- action.ps1
+++ action.ps1
@@ -267,7 +267,7 @@ try
                 return;
             }
 
-            $patterns = $patterns.Split(" ", "RemoveEmptyEntries")
+            $patterns = $patterns.Split(" ", 100, "RemoveEmptyEntries")
 
             foreach ($pattern in $patterns)
             {
